5e0b06d48997b2577e528a3c53917af14c7a6fdc
Manish Lakhara <manishl@meditab.com>
2024-04-19 16:43:32 +0530

    craete pages and routes

--- Travel_Buddy/src/App.jsx
+++ Travel_Buddy/src/App.jsx
@@ -1,14 +1,27 @@
-import { useState } from 'react'
+import React from 'react';
 import './index.css';
+import {BrowserRouter, Routes, Route} from 'react-router-dom'; 
+import Home from './pages/Home';
+import About from './pages/About';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import Dashboard from './pages/Dashboard';
+import Journal from './pages/Journal';
 
 function App() {
 
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-    </>
+   <BrowserRouter>
+   <Routes>
+    <Route path='/' element={<Home/>}/>
+    <Route path='/About' element={<About/>}/>
+    <Route path='/SignIn' element={<SignIn/>}/>
+    <Route path='/SignUp' element={<SignUp/>}/>
+    <Route path='/Dashboard' element={<Dashboard/>}/>
+    <Route path='/Journal' element={<Journal/>}/>
+
+   </Routes>
+   </BrowserRouter>
   )
 }
 
