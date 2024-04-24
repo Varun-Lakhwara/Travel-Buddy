e1b9ddc3875fe70c32037c7f1c56d3202a335c8b
Manish Lakhara <manishl@meditab.com>
2024-04-24 09:29:13 +0530

    complete user image upload functionality

--- Travel_Buddy/src/components/DashSideBar.jsx
+++ Travel_Buddy/src/components/DashSideBar.jsx
@@ -18,16 +18,16 @@ export default function DashSideBar() {
     <Sidebar className="w-full md:w-56">
       <Sidebar.Items>
         <Sidebar.ItemGroup>
-          <Link>
+        <Link to="/dashboard?tab=profile">
             <Sidebar.Item
               active={tab === "profile"}
               icon={HiUser}
               label={"User"}
               labelColor="dark"
-            >
+              as='button'>
               Profile
             </Sidebar.Item>
-          </Link>
+            </Link>
           <Sidebar.Item icon={HiArrowSmRight} className="cursor-pointer">
             Sign Out
           </Sidebar.Item>
