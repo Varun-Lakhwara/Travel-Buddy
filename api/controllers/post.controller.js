181c7eec308311680ca4f15cfa13eeb9d77c970e
Manish Lakhara <manishl@meditab.com>
2024-04-27 20:13:28 +0530

    create get post api route

--- api/controllers/post.controller.js
+++ api/controllers/post.controller.js
@@ -2,7 +2,7 @@ const errorHandler = require("../utils/error");
 const Post = require("../models/post.model.js"); 
 
 const createjournal = async (req,res,next) =>{
- if(!req.user.isAdmin){
+   if (req.user && req.user.isAdmin){
     return next(errorHandler(403, 'You are not allowed to create a post'));
  }   
  if(!req.body.title || !req.body.content){
@@ -23,5 +23,49 @@ const createjournal = async (req,res,next) =>{
         next(error);
      }
 };
+
+const getposts = async ( req, res, next ) => {
+   try {
+      const startIndex = parseInt(req.query.startIndex) || 0;
+      const limit = parseInt(req.query.limit) || 9;
+      const sortDirection = req.query.sort.order === 'asc' ? 1 : -1;
+      const posts = await Post.find(
+         ...(req.query.userId && { userId: req.query.userId }),
+         ...(req.query.category && { category: req.query.category }),
+         ...(req.query.slug && { slug: req.query.slug }),
+         ...(req.query.postId && { _id: req.query.postId }),
+         ...(req.query.searchTerm && {
+            $or: [
+               {title : { $regex: req.query.searchTerm, $options: 'i'}},
+               {content : { $regex: req.query.searchTerm, $options: 'i'}},
+            ],
+         }),
+      ).sort({updatedAt: sortDirection}).skip(startIndex).limit(limit);
+
+      const totalPost = await Post.countDoucuments();
+
+      const now = new Date();
+      const oneMonth = new Date(
+         now.getFullYear(),
+         now.getMonth() -1,
+         now.getDate()
+      );
+      
+      const lastMonthPost = await Post.countDoucuments({
+         createdAt: {
+            $gte: oneMonth,
+         }
+      });
+
+      res.status(200).json({
+         posts,
+         totalPost,
+         lastMonthPost
+      });
+
+   } catch (error) {
+      next(error);
+   }
+}
 
 module.exports = createjournal;
--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const verifyToken = require('../utils/error.js');
 const createjournal = require('../controllers/post.controller.js');
+const getposts = require('../controllers/post.controller.js');
 
 const router = express.Router();
 
 router.post('/createjournal', verifyToken, createjournal);
+router.get('/getpost',getposts)
 
 module.exports = router;
 
