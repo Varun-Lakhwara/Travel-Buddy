e427ea5407a0aa6e7a5f2c86f8ab6c39d1b56b23
Manish Lakhara <manishl@meditab.com>
2024-05-13 14:51:11 +0530

    add like functionality to the components

--- Travel_Buddy/src/components/Comment.jsx
+++ Travel_Buddy/src/components/Comment.jsx
@@ -1,39 +1,65 @@
-import React, { useEffect, useState } from 'react';
-import moment from 'moment';
+import React, { useEffect, useState } from "react";
+import moment from "moment";
+import { FaThumbsUp } from "react-icons/fa";
+import { useSelector } from "react-redux";
 
-export default function Comment({comment}) {
-const [ user, setUser ] = useState({});
+export default function Comment({ comment, onLike }) {
+  const currentUser = useSelector((state) => state.user);
+  const [user, setUser] = useState({});
 
-    useEffect(() => {
-        const getUsers = async() => {
-            try {
-                const res = await fetch (`/api/user/${comment.userId}`);
-                const data = await res.json();
-                if(res.ok){
-                    setUser(data);
-                }
-
-                
-            } catch (error) {
-                console.log(error.message);
-            }
+  useEffect(() => {
+    const getUsers = async () => {
+      try {
+        const res = await fetch(`/api/user/${comment.userId}`);
+        const data = await res.json();
+        if (res.ok) {
+          setUser(data);
         }
-        getUsers();
-    }, [comment]
-)
+      } catch (error) {
+        console.log(error.message);
+      }
+    };
+    getUsers();
+  }, [comment]);
 
   return (
-    <div className='flex border-b p-4 dark:border-gray-600 text-sm'>
+    <div className="flex border-b p-4 dark:border-gray-600 text-sm">
       <div className="flex-shrink-0 mr-3">
-        <img className="w-10 h-10 rounded-full bg-gray-200" src={user.profilePicture} alt={user.username}/>
+        <img
+          className="w-10 h-10 rounded-full bg-gray-200"
+          src={user.profilePicture}
+          alt={user.username}
+        />
       </div>
       <div className="flex-1">
         <div className="flex items-center mb-1">
-            <span className='font-bold mr-1 text-xs truncate'>{user ? `@${user.username}` : 'anonymous user'}</span>
-            <span className='text-gray-500 text-xs '>{moment(comment.createdAt).fromNow()}</span>
+          <span className="font-bold mr-1 text-xs truncate">
+            {user ? `@${user.username}` : "anonymous user"}
+          </span>
+          <span className="text-gray-500 text-xs ">
+            {moment(comment.createdAt).fromNow()}
+          </span>
         </div>
-        <p className='text-gray-500 pb-2'>{comment.content}</p>
+        <p className="text-gray-500 pb-2">{comment.content}</p>
+        <div className="flex items-center pt-2 text-xs border-t dark:border-gray-700 max-w-fit gap-2">
+          <button
+            type="button"
+            onClick={() => onLike(comment._id)}
+            className={`text-gray-400 hover:text-blue-500
+            ${
+              currentUser &&
+              comment.Likes.includes(currentUser._id) &&
+              "text-blue-500"
+            }`}
+          >
+            <FaThumbsUp className="text-sm " />
+          </button>
+          <p className="text-gray-400">
+            {comment.numberOflikes > 0 && comment.numberOflikes + " " + (
+            comment.numberOflikes === 1 ? 'like' : 'likes'
+          ) }</p>
+        </div>
       </div>
     </div>
-  )
+  );
 }
--- api/routes/comment.route.js
+++ api/routes/comment.route.js
@@ -2,10 +2,12 @@ const express = require('express');
 const verifyToken = require('../utils/error.js');
 const createComment = require('../controllers/comment.controller.js');
 const getPostComments = require('../controllers/comment.controller.js');
+const likeComment = require('../controllers/comment.controller.js');
 
 const router = express.Router();
 
 router.post('/create', verifyToken, createComment);
 router.get('/getPostComments/:postId', getPostComments);
+router.put('/likeComment/:commentId', verifyToken, likeComment);
 
 module.exports = router;
